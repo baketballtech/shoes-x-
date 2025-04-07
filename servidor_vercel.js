f49a16ddc856a8c9c479611d532e1c47878c5226
Janilson <janilsonp224@gmail.com>
2025-04-07 19:36:59 -0100

    Update servidor_vercel.js

--- servidor_vercel.js
+++ servidor_vercel.js
@@ -7,7 +7,7 @@ const { Client } = pkg; // Desestruturando Client do pacote
 const app = express();
 //codigo 3
 // Configuração do middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use(helmet());
 
