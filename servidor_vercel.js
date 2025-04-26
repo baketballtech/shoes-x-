64779cc2e8da5e576bc81f3e0c9cd9d0995a4c2f
Janilson <janilsonp224@gmail.com>
2025-04-26 13:46:00 -0100

    Update servidor_vercel.js

--- servidor_vercel.js
+++ servidor_vercel.js
@@ -31,7 +31,7 @@ client.connect()
 app.get("/pegar", async (req, res) => {
   try {
     const result = await client.query(
-      "SELECT * FROM sapatos ORDER BY CASE WHEN marca = 'Nike' THEN 1 WHEN marca = 'Adidas' THEN 2 WHEN marca = 'Puma' THEN 3 when marca='Gucci' then 4 when marca='Louis Vuitton' then 5 ELSE 6 END, marca;"
+      "SELECT * FROM sapatos WHERE loja IS NOT NULL AND loja <> '' AND tipo = 'loja' ORDER BY CASE WHEN marca = 'Nike' THEN 1 WHEN marca = 'Adidas' THEN 2 WHEN marca = 'Puma' THEN 3 WHEN marca = 'Gucci' THEN 4 WHEN marca = 'Louis Vuitton' THEN 5 ELSE 6 END, marca"
     );
     res.json(result.rows);
   } catch (error) {
